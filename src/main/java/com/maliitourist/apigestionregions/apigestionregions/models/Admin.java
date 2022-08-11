3342f1914491f92114be76809b094ac56f7db7af
Hawa656 <107039828+Hawa656@users.noreply.github.com>
2022-08-11 16:14:11 +0000

    premier commit 1614

--- src/main/java/com/maliitourist/apigestionregions/apigestionregions/models/Admin.java
+++ src/main/java/com/maliitourist/apigestionregions/apigestionregions/models/Admin.java
@@ -1,5 +1,32 @@
 package com.maliitourist.apigestionregions.apigestionregions.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.*;;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class Admin {
+
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id_admin;
+    private String nom;
+    private String prenom;
+
+    @OneToMany(mappedBy = "admin")
+    Set<Region> region;
 
 }
