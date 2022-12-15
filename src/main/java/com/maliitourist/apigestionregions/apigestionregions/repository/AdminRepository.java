30cea2025742a32ff20a7ed2f134fdaccca3d8fd
BalloIbrahima <ibrahimaballo01@gmail.com>
2022-12-15 16:40:35 +0000

    add userdetailimpl class

--- src/main/java/com/maliitourist/apigestionregions/apigestionregions/repository/AdminRepository.java
+++ src/main/java/com/maliitourist/apigestionregions/apigestionregions/repository/AdminRepository.java
@@ -1,9 +1,13 @@
 package com.maliitourist.apigestionregions.apigestionregions.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.maliitourist.apigestionregions.apigestionregions.models.Admin;
 
 public interface AdminRepository extends JpaRepository<Admin, Integer> {
+
+    Optional<Admin> findByUsername(String username);
 
 }
